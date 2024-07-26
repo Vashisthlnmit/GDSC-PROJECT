1bb65c8aea7976ce0face106787fe25e71d9bbdd
Vashisthlnmit <vashisthkumar54@gmail.com>
2024-07-26 10:14:51 +0530

    second commit

--- Frontend/gdsc-front/vite.config.js
+++ Frontend/gdsc-front/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:"http://localhost:8708/",
+        target:"https://gdsc-project-1.onrender.com/",
         changeOrigin:true,
         rewrite:(path)=>path.replace(/^\/api/, '')
       }
